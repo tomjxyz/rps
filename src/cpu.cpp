ce24ec4c7399f165b401a3383f8b76d66e9dac9b
Tom <wizard@tomj.xyz>
2024-03-23 20:27:30 +0000

    Game loop implemented Fixed next not storing the choice of last roll

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -3,7 +3,7 @@
 #include "../include/cpu.h"
 Cpu::Cpu() {
     wins = 0;
-    round = 1;
+    round = 0;
     opts = {'r','p','s'};
 
     std::random_device rd{};
@@ -14,10 +14,35 @@ Cpu::Cpu() {
 }
 
 char Cpu::next() {
-    return opts[three(mt)];
+    choice = opts[three(mt)]; 
+    return choice;
 }
 
 bool Cpu::play(char userChoice) {
-    return userChoice;
+    switch (userChoice)
+    {
+    case 'r':
+        if (choice == 'r' || choice == 'p')
+            return false;
+        else
+            return true;
+        break;
+    case 'p':
+        if (choice == 's' || choice == 'p')
+            return false;
+        else
+            return true;
+        break;
+    case 's':
+        if (choice == 'r' || choice == 's')
+            return false;
+        else
+            return true;
+        break;
+    default:
+        std::cout << "Enter a valid choice (r p s)\n";
+        return false;
+        break;
+    }
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <unistd.h>
+#include <termio.h>
 #include "../include/cpu.h"
 
 int main() {
     Cpu pc{};
-    for (int i = 0; i < 1000; i++){
-        std::cout << pc.next();
+    char userChoice{};
+    bool win{};
+
+    // Turn off terminal buffering
+    struct termios t;
+    tcgetattr(STDIN_FILENO, &t);
+    t.c_lflag &= ~ICANON;
+    tcsetattr(STDIN_FILENO, TCSANOW, &t);
+
+    std::cout << "--- Rock Paper Scissors ---\nPress CRTL+C to exit\n";
+    while (true){
+        std::cout << "Enter Move: ";
+        userChoice = std::getchar();
+        win = pc.play(userChoice);
+        if (win)
+            std::cout << "\nWin!\n";
+        else
+            std::cout << "\nLose\n";
+        
+        pc.next();
     }
-    std::cout << std::endl;
 
     return 0;
 }
